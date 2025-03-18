e62c9387be412aa85174fc5bb26466b05f58aff5
HarshalGunjalOp <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>
2025-03-18 18:01:34 +0530

    tmux-sessionizer in nvim didn't work

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,5 +6,4 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<C-f>", "<cmd>silent !tmux neww sessionizer<CR>")
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
